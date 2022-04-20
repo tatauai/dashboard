2fbdc8223cc88f540436a80dc693764ea1d8a1c6
Talita de Freitas Silva <talitafs98@hotmail.com>
2022-04-20 16:08:16 -0300

    inclusão da area para cadastrar indicador

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,7 +1,15 @@
 let content = document.getElementById('ajax-content')
-
-function fetchContent(){
-  fetch('')
-
+function fetchContent(el){
+  let view = el.getAttribute('a-view')
+  let folder = el.getAttribute('a-folder')
+  fetch(`/ajax/${folder}/${view}.html`)
+  .then(response => {
+    let html = response.text();
+    return html
+  })
+  .then(html => {
+    console.log(html);
+    content.innerHTML = '<p>Teste</p>'
+  })
   
 }
